dec4b07fac1631529e581a488c97a2509be07e2f
Vlad70003 <74984024+Vlad70003@users.noreply.github.com>
2021-01-30 23:00:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+
+// всплывающее меню секции offers
+let settingGear = document.querySelector(".settings__gear");
+let settingList = document.querySelector(".settings__list");
+
+settingGear.addEventListener("mouseover", function(event){
+    if(event){
+        settingList.style.display = "block";
+    }  
+})
+settingGear.addEventListener("mouseout", function(event){
+    if(event){
+        settingList.style.display = "none";
+    }  
+})
+
+// всплывающее меню Гамбургер
+let hamburger = document.querySelector(".hamburger");
+let hideMenu = document.querySelector(".hide-menu");
+let hideMenuClose = document.querySelector(".hide-menu__close");
+
+hamburger.addEventListener("click", function(event){
+    if(event){
+        hideMenu.style.display = "block";
+    }  
+})
+
+hideMenuClose.addEventListener("click", function(event){
+    if(event){
+        hideMenu.style.display = "none";
+    }  
+})
