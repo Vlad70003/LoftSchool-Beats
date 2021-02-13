f813fef252864b60fb184279c50548935070535d
Vlad70003 <74984024+Vlad70003@users.noreply.github.com>
2021-02-13 15:05:02 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 // всплывающее меню секции offers
-let offersList = document.querySelector(".offers__list");
-
-offersList.addEventListener("click", function(event) {
-    let $this = $(event.target);
-    
-
-    if($this.hasClass("block")){
-        $this.removeClass("block");
+let offersList = document.querySelector(".offers");
+ 
+offersList.addEventListener("mouseover", function(event) {
+    let cursor = $(event.target);
+    let findBlock = cursor.children(".settings__list");
+    if (findBlock){
+        let settingsGear = findBlock.closest(".settings__gear");
+        settingsGear.addClass("block");
     }
-    else{
-        let conteiner = $this.closest(".offers__list");
-        let findClass = conteiner.find(".settings__gear");
-        findClass.removeClass("block");
-        $this.addClass("block");
-    }
-    
+
+})
+offersList.addEventListener("mouseout", function(event) {
+    let cursor = $(event.target);
+    let findBlock = cursor.find(".settings__gear");
+    findBlock.removeClass("block");
 })
     
 // всплывающее меню Гамбургер
