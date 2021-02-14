ba386fe1f4d1b11a4c5a7b334d838178a8212f1d
Vlad70003 <74984024+Vlad70003@users.noreply.github.com>
2021-02-14 17:57:47 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -105,6 +105,73 @@ $(".arrow-right").on("click", e => {
     e.preventDefault();
     slider.goToNextSlide();
 })
+
+    //Цвета
+let blockWidth = item => {
+    let reqItemWidth = 0;
+    let screenWidth = $(window).width();
+    let conteiner = item.closest(".colors__list");
+    let titleBlocks = conteiner.find(".color__link");
+    let titleWidth = titleBlocks.width() * titleBlocks.length;
+
+    let textConteiner = item.find(".colors__hidden-conteiner");
+    let paddingLeft = parseInt(textConteiner.css("padding-left"));
+    let paddingRight = parseInt(textConteiner.css("padding-right"));
+
+    let isMobile = window.matchMedia("(max-width: 850px)").matches;
+
+    if (isMobile){
+        reqItemWidth =  screenWidth - titleWidth;
+    }
+    else{
+        reqItemWidth = 500;
+    }
+
+    return {
+        conteiner: reqItemWidth,
+        textConteiner: reqItemWidth - paddingLeft - paddingRight
+    }
+    
+}
+
+let openHiddenConteiner = conteiner => {
+    conteiner.addClass("active");
+    let hiddenConteiner = conteiner.find(".colors__hidden-conteiner");
+    let reqWidth = blockWidth(conteiner);
+    let textBlock = conteiner.find(".colors__hidden-conteiner");
+
+    textBlock.width(reqWidth.textConteiner);
+    hiddenConteiner.width(reqWidth.conteiner);
+    hiddenConteiner.width(reqWidth);
+    hiddenConteiner.css("opacity", "0.8");
+    
+}
+let closeHiddenConteiner = conteiner => {
+    let colorsList = conteiner.closest(".colors__list");
+    let colorItem = colorsList.find(".color__item");
+    colorItem.removeClass("active");
+    let hiddenConteiner = colorsList.find(".colors__hidden-conteiner");
+    hiddenConteiner.width(0);
+    hiddenConteiner.css("opacity", "0");
+}
+
+$(".colors__list").on("click", (e) => {
+    e.preventDefault();
+    let $target = $(e.target);
+    let conteiner = $target.closest(".color__item");
+    
+    if (conteiner.hasClass("active")){      
+        closeHiddenConteiner(conteiner);
+    }
+    else{
+        
+        closeHiddenConteiner(conteiner);
+        openHiddenConteiner(conteiner);
+    }
+    
+})
+
+
     //Модальное окно формы
 
 const validateFilds = (form, fieldsArray) => {
